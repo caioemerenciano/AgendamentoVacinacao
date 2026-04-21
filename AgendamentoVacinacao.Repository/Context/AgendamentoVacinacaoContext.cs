7becc534f2dbdbc6159552d366fff06283ceec3b
Caio Marques <caio.m.e@hotmail.com>
2026-04-21 13:38:16 -0300

    feat: cria pasta Context com configuração com as entidades Paciente e Agendamento
    -Adiciona Referência com projeto Entity
    -Configura Entity framework para carregar as classes do "Map"

---
+++ AgendamentoVacinacao.Repository/Context/AgendamentoVacinacaoContext.cs
@@ -0,0 +1,21 @@
+﻿using AgendamentoVacinacao.Entity.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgendamentoVacinacao.Repository.Context;
+
+public class AgendamentoVacinacaoContext : DbContext
+{
+    public AgendamentoVacinacaoContext(DbContextOptions<AgendamentoVacinacaoContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Paciente> Pacientes { get; set; }
+    public DbSet<Agendamento> Agendamentos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AgendamentoVacinacaoContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
