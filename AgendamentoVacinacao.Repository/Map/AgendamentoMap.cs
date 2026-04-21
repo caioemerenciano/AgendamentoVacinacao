8c477f4b7954fcb01e66af33ef3a878322ea85b8
Caio Marques <caio.m.e@hotmail.com>
2026-04-21 14:46:10 -0300

    refactor(Map): ajusta os nomes das tabelas e colunas para o do desafio

--- AgendamentoVacinacao.Repository/Map/AgendamentoMap.cs
+++ AgendamentoVacinacao.Repository/Map/AgendamentoMap.cs
@@ -11,16 +11,36 @@ public class AgendamentoMap : IEntityTypeConfiguration<Agendamento>
         builder.ToTable("tb_agendamento");
 
         builder.HasKey(a => a.Id);
-        builder.Property(a => a.Id).HasColumnName("id");
+
+        builder.Property(a => a.Id)
+            .HasColumnName("id_agendamento")
+            .ValueGeneratedNever();
 
         builder.Property(a => a.IdPaciente)
             .HasColumnName("id_paciente")
             .IsRequired();
 
-        builder.Property(a => a.DataAgendamento)
-            .HasColumnName("data_agendamento")
+        builder.Property(a => a.DataAgendamento).HasColumnName("dat_agendamento")
+            .HasColumnType("date")
             .IsRequired();
 
+        builder.Property(a => a.HoraAgendamento)
+            .HasColumnName("hor_agendamento")
+            .HasColumnType("time")
+            .IsRequired();
 
+        builder.Property(a => a.Status)
+            .HasColumnName("dsc_status")
+            .HasMaxLength(50).IsRequired()
+            .IsUnicode(false);
+
+        builder.Property(a => a.DataCriacao)
+            .HasColumnName("dat_criacao")
+            .HasColumnType("datetime").IsRequired();
+
+        builder.HasOne(a => a.Paciente)
+               .WithMany(p => p.Agendamentos)
+               .HasForeignKey(a => a.IdPaciente)
+               .HasConstraintName("fk_agendamento_paciente");
     }
 }
