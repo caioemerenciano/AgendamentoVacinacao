fa74b2afbe44d19f69f38527deae8adcd6c6abe3
Caio Marques <caio.m.e@hotmail.com>
2026-04-21 14:22:04 -0300

    fix(entity): corrigir tipos de propriedade na entidade Paciente
    -Remove o Id do construtor
    -Alteração do Id de Guid para Int
    -Alteração do DataNascimento de DateOnly para DateTime

--- AgendamentoVacinacao.Entity/Entities/Paciente.cs
+++ AgendamentoVacinacao.Entity/Entities/Paciente.cs
@@ -6,14 +6,15 @@ public class Paciente
 
     public Paciente(Guid id, string nome, DateOnly dataNascimento)
     {
-        Id = id;
         Nome = nome;
         DataNascimento = dataNascimento;
+        DataCriacao = DateTime.Now;
     }
 
     public Guid Id { get; private set; }
     public string Nome { get; private set; }
-    public DateOnly DataNascimento { get; private set; }
+    public DateTime DataNascimento { get; private set; }
+    public DateTime DataCriacao { get; private set; }
 
     public virtual ICollection<Agendamento> Agendamentos { get; private set; } = new List<Agendamento>();
 }
--- AgendamentoVacinacao.Repository/Map/AgendamentoMap.cs
+++ AgendamentoVacinacao.Repository/Map/AgendamentoMap.cs
@@ -1,11 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AgendamentoVacinacao.Entity.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace AgendamentoVacinacao.Repository.Map;
 
-internal class AgendamentoMap
+public class AgendamentoMap : IEntityTypeConfiguration<Agendamento>
 {
+    public void Configure(EntityTypeBuilder<Agendamento> builder)
+    {
+        builder.ToTable("tb_agendamento");
+
+        builder.HasKey(a => a.Id);
+        builder.Property(a => a.Id).HasColumnName("id");
+
+        builder.Property(a => a.IdPaciente)
+            .HasColumnName("id_paciente")
+            .IsRequired();
+
+        builder.Property(a => a.DataAgendamento)
+            .HasColumnName("data_agendamento")
+            .IsRequired();
+
+
+    }
 }
