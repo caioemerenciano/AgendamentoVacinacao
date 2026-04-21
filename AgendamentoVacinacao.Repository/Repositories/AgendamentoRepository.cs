f05cd614a9ab3cf59523fe0d3f86a3ef43b951bb
Caio Marques <caio.m.e@hotmail.com>
2026-04-21 14:00:33 -0300

    feat: adiciona o Repository de Agendamento
    -Injeta dependências da configuração do context
    -Adiciona contagem de agendamentos por dia para validar no Business
    -Adiciona contagem de agendamentos por horário para validar no Business

--- AgendamentoVacinacao.Repository/Repositories/AgendamentoRepository.cs
+++ AgendamentoVacinacao.Repository/Repositories/AgendamentoRepository.cs
@@ -1,11 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AgendamentoVacinacao.Repository.Context;
+using AgendamentoVacinacao.Repository.Interface;
+using AgendamentoVacinacao.Entity.Entities;
+using AgendamentoVacinacao.Repository.Interface.IRepositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace AgendamentoVacinacao.Repository.Repositories;
 
-internal class AgendamentoRepository
+public class AgendamentoRepository : IAgendamentoRepository
 {
+    private readonly AgendamentoVacinacaoContext _context;
+
+    public AgendamentoRepository(AgendamentoVacinacaoContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<int> ContarAgendamentosPorDiaAsync(DateTime data)
+    {
+        return await _context.Agendamentos
+            .CountAsync(a => a.DataAgendamento.Date == data.Date);
+    }
+
+    public async Task<int> ContarAgendamentosPorHorarioAsync(DateTime dataHoraExata)
+    {
+        return await _context.Agendamentos
+            .CountAsync(a => a.DataAgendamento == dataHoraExata);
+    }
+
+
+    public async Task AdicionarAsync(Agendamento agendamento)
+    {
+        await _context.Agendamentos.AddAsync(agendamento);
+    }
+
+    public async Task SalvarAlteracoesAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
 }
