9f330c2593c52b6146b4f8cfc7f2c12e12a52c5c
Caio Marques <caio.m.e@hotmail.com>
2026-04-21 16:35:21 -0300

    feat(dto): adiciona AgendamentoResponse
    -Adiciona DTO de Response com nome do paciente e status

---
+++ AgendamentoVacinacao.Entity/DTOs/Response/AgendamentoResponse.cs
@@ -0,0 +1,11 @@
+﻿using AgendamentoVacinacao.Entity.Enums;
+
+namespace AgendamentoVacinacao.Entity.DTOs.Response;
+
+public record AgendamentoResponse(
+    int Id,
+    int IdPaciente,
+    string NomePaciente,
+    DateTime DataAgendamento,
+    TimeSpan HoraAgendamento,
+    StatusAgendamento Status);
