3821d19db5674bfa5bd3be9f3de4a7c99b9b56aa
Caio Marques <caio.m.e@hotmail.com>
2026-04-21 17:03:43 -0300

    feat(validator): adicionar validators para CriarPacienteRequest e CriarAgendamentoRequest
    -Valida nome e data de nascimento do paciente (presença, tamanho, data no passado)
    -Valida agendamento (IdPaciente > 0, data não passada, hora em intervalos de 1h entre 08:00–17:00)

---
+++ Validators/Validators/AgendamentoRequestValidator.cs
@@ -0,0 +1,23 @@
+﻿using AgendamentoVacinacao.Entity.DTOs.Request;
+using FluentValidation;
+
+namespace AgendamentoVacinacao.Validator.Validators;
+
+public class AgendamentoRequestValidator : AbstractValidator<CriarAgendamentoRequest>
+{
+    public AgendamentoRequestValidator()
+    {
+        RuleFor(x => x.IdPaciente)
+            .GreaterThan(0).WithMessage("Id do paciente inválido.");
+
+        RuleFor(x => x.DataAgendamento)
+            .NotEmpty().WithMessage("A data do agendamento é obrigatória.")
+            .Must(d => d.Date >= DateTime.Now.Date).WithMessage("Não é possível agendar para uma data passada.");
+
+        RuleFor(x => x.HoraAgendamento)
+            .Must(h => h.Minutes == 0 && h.Seconds == 0)
+            .WithMessage("Os agendamentos devem ser feitos em intervalos exatos de 1 hora (ex: 08:00, 09:00).")
+            .Must(h => h.Hours >= 8 && h.Hours <= 17)
+            .WithMessage("O horário de vacinação é das 08:00 às 17:00.");
+    }
+}
---
+++ Validators/Validators/PacienteRequestValidator.cs
@@ -0,0 +1,19 @@
+﻿using AgendamentoVacinacao.Entity.DTOs.Request;
+using FluentValidation;
+
+namespace AgendamentoVacinacao.Validators.Validators;
+
+public class PacienteRequestValidator : AbstractValidator<CriarPacienteRequest>
+{
+    public PacienteRequestValidator()
+    {
+        RuleFor(x => x.Nome)
+            .NotEmpty().WithMessage("O nome do paciente é obrigatório.")
+            .MinimumLength(3).WithMessage("O nome deve ter no mínimo 3 caracteres.")
+            .MaximumLength(150).WithMessage("O nome deve ter no máximo 150 caracteres.");
+
+        RuleFor(x => x.DataNascimento)
+            .NotEmpty().WithMessage("A data de nascimento é obrigatória.")
+            .LessThan(DateTime.Now).WithMessage("A data de nascimento deve ser no passado.");
+    }
+}
