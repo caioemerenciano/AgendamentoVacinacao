d77a8347624c22b9b6214fc5823d4d8b68af347b
Caio Marques <caio.m.e@hotmail.com>
2026-04-21 12:17:56 -0300

    feat: Cria projeto de interface do repository
    -Cria contratos de métodos essenciais para validar regras de negócio de capacidade

---
+++ AgendamentoVacinacao.Repository.Interface/IRepositories/IAgendamentoRepository.cs
@@ -0,0 +1,11 @@
+﻿using AgendamentoVacinacao.Entity.Entities;
+
+namespace AgendamentoVacinacao.Repository.Interface.IRepositories;
+
+public interface IAgendamentoRepository
+{
+    Task<int> ContarAgendamentosPorDiaAsync(DateTime data);
+    Task<int> ContarAgendamentosPorHorarioAsync(DateTime dataHoraExata);
+
+    Task AdicionarAsync(Agendamento agendamento);
+}
