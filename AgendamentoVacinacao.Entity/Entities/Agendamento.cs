bb16b696ce736b7da902ed4fab67b11bdfcb0f01
Caio Marques <caio.m.e@hotmail.com>
2026-04-21 11:46:10 -0300

    feat: implementação da entidade Agendamento com as propriedades.
    -Criação de  Construtor com Id, IdPaciente, DataAgendamento e CriadoEm

--- AgendamentoVacinacao.Entity/Entities/Agendamento.cs
+++ AgendamentoVacinacao.Entity/Entities/Agendamento.cs
@@ -1,6 +1,24 @@
-﻿namespace AgendamentoVacinacao.Entity.Entities;
+﻿using System;
+
+namespace AgendamentoVacinacao.Entity.Entities;
 
 public class Agendamento
 {
+    protected Agendamento() { }
 
+    public Agendamento(Guid id, Guid idPaciente, DateTime dataAgendamento)
+    {
+        Id = id;
+        IdPaciente = idPaciente;
+        DataAgendamento = dataAgendamento;
+        CriadoEm = DateTime.Now; 
+    }
+
+    public Guid Id { get; private set; }
+    public Guid IdPaciente { get; private set; }
+    public DateTime DataAgendamento { get; private set; }
+    public DateTime CriadoEm { get; private set; }
+
+
+    public virtual Paciente? Paciente { get; private set; }
 }
