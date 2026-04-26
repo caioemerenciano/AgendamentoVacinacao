c1d84d4447efaf0328149f87377bf99cb4b2a7a6
Caio Marques <caio.m.e@hotmail.com>
2026-04-26 19:01:09 -0300

    feat: ignorar agendamentos cancelados no cálculo de disponibilidade de slots

--- AgendamentoVacinacao.Repository/Repositories/AgendamentoRepository.cs
+++ AgendamentoVacinacao.Repository/Repositories/AgendamentoRepository.cs
@@ -1,5 +1,6 @@
 using AgendamentoVacinacao.Repository.Context;
 using AgendamentoVacinacao.Entity.Entities;
+using AgendamentoVacinacao.Entity.Enums;
 using AgendamentoVacinacao.Repository.Interface.IRepositories;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,7 +16,7 @@ public class AgendamentoRepository : IAgendamentoRepository
         _context = context;
     }
     public async Task<int> ContarAgendamentosPorDiaAsync(DateTime data) => 
-        await _context.Agendamentos.CountAsync(a => a.DataAgendamento.Date == data);
+        await _context.Agendamentos.CountAsync(a => a.DataAgendamento.Date == data && a.Status != StatusAgendamento.Cancelado);
     
     public async Task<int> ContarAgendamentosPorHorarioAsync(DateTime data, TimeSpan hora)
     {
@@ -25,7 +26,10 @@ public class AgendamentoRepository : IAgendamentoRepository
         var limiteSuperior = hora.Add(TimeSpan.FromHours(1));
         if (limiteSuperior > new TimeSpan(23, 59, 59)) limiteSuperior = new TimeSpan(23, 59, 59);
 
-        return await _context.Agendamentos.CountAsync(a => a.DataAgendamento == data && a.HoraAgendamento > limiteInferior && a.HoraAgendamento < limiteSuperior);
+        return await _context.Agendamentos.CountAsync(a => a.DataAgendamento == data 
+                                                       && a.HoraAgendamento > limiteInferior 
+                                                       && a.HoraAgendamento < limiteSuperior
+                                                       && a.Status != StatusAgendamento.Cancelado);
     }
    
     public async Task<IEnumerable<Agendamento>> ObterTodosAsync() =>
@@ -57,7 +61,8 @@ public class AgendamentoRepository : IAgendamentoRepository
             .AnyAsync(a => a.DataAgendamento == data
                         && a.HoraAgendamento > limiteInferior
                         && a.HoraAgendamento < limiteSuperior
-                        && a.Id != agendamentoIdIgnorado);
+                        && a.Id != agendamentoIdIgnorado
+                        && a.Status != StatusAgendamento.Cancelado);
     }
 
 
