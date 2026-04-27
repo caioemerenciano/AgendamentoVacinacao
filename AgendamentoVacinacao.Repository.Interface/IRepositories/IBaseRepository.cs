daa83cfa46aa1d3ab93c893702a0fc0324efdfe2
Caio Marques <caio.m.e@hotmail.com>
2026-04-26 21:13:47 -0300

    feat: implementar infraestrutura de repositório genérico (IBaseRepository e BaseRepository)

---
+++ AgendamentoVacinacao.Repository.Interface/IRepositories/IBaseRepository.cs
@@ -0,0 +1,11 @@
+namespace AgendamentoVacinacao.Repository.Interface.IRepositories;
+
+public interface IBaseRepository<T> where T : class
+{
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T?> GetByIdAsync(int id);
+    Task AddAsync(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+    Task SaveChangesAsync();
+}
